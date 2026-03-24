fe4a60eadc475d96f95b6371135b2c3be79c76de
Erick Cruz <erickcruz147@gmail.com>
2026-03-24 15:10:19 -0500

    change github username

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import vue from '@vitejs/plugin-vue'
-import federation from '@originjs/vite-plugin-federation'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import vue from "@vitejs/plugin-vue";
+import federation from "@originjs/vite-plugin-federation";
 
-// Change 'erickcguz' to your actual GitHub username everywhere below
-const GITHUB_USER = 'erickcguz'
+const GITHUB_USER = "hoizard";
 
 export default defineConfig({
   plugins: [
     react(),
     vue(),
     federation({
-      name: 'portfolio_shell',
+      name: "portfolio_shell",
       remotes: {
         mfe_vue_tasks: `https://${GITHUB_USER}.github.io/mfe-vue-tasks/dist/remoteEntry.js`,
         mfe_angular_counter: `https://${GITHUB_USER}.github.io/mfe-angular-counter/dist/remoteEntry.js`,
         mfe_react_weather: `https://${GITHUB_USER}.github.io/mfe-react-weather/dist/remoteEntry.js`,
       },
-      shared: ['react', 'react-dom', 'vue'],
+      shared: ["react", "react-dom", "vue"],
     }),
   ],
   build: {
-    target: 'esnext',
+    target: "esnext",
     minify: false,
   },
   base: `https://${GITHUB_USER}.github.io/portfolio-shell/`,
-})
+});
